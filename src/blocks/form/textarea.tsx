62af92d7e6a70eee94e85fbd11b1aa227e338aec
Hiroshi Urabe <urabe@ham.works>
2020-03-06 16:03:58 +0900

    icon

--- src/blocks/form/textarea.tsx
+++ src/blocks/form/textarea.tsx
@@ -33,12 +33,21 @@ registerBlockType<{ label: string; name: string; required: boolean }>(
 		icon: () => (
 			<svg
 				xmlns="http://www.w3.org/2000/svg"
+				enableBackground="new 0 0 24 24"
 				height="24"
 				viewBox="0 0 24 24"
 				width="24"
 			>
-				<path d="M0 0h24v24H0z" fill="none" />
-				<path d="M21 3.01H3c-1.1 0-2 .9-2 2V9h2V4.99h18v14.03H3V15H1v4.01c0 1.1.9 1.98 2 1.98h18c1.1 0 2-.88 2-1.98v-14c0-1.11-.9-2-2-2zM11 16l4-4-4-4v3H1v2h10v3z" />
+				<g>
+					<rect fill="none" height="24" width="24" />
+				</g>
+				<g>
+					<g>
+						<g>
+							<path d="M2.5,4v3h5v12h3V7h5V4H2.5z M21.5,9h-9v3h3v7h3v-7h3V9z" />
+						</g>
+					</g>
+				</g>
 			</svg>
 		),
 		title: __('Textarea', 'form-field-blocks'),
